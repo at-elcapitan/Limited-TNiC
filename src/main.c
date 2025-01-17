98240eb7c70a74ac9971921c3795c4a1cdfbae2a
at-elcapitan <gameforceman1808@gmail.com>
2025-01-17 16:21:14 +0200

    changing LICENSE

--- src/main.c
+++ src/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
 
 int main(int argc, char const *argv[]) {
-    printf("TNiC first commit\n");
+    puts("AT PROJECT Limited, 2022 - 2025; ATNiC-v0.0.1a");
+    puts("Product licensed by GPLv3, file `LICENSE`");
+    puts("This is a prototype version and should not be used in production environments");
     return 0;
 }
